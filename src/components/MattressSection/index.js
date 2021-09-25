ba22df701961fbc2bf36b94670381104726b6354
Juan Manrique <juanfmf2209@hotmail.com>
2021-09-24 22:40:51 -0500

    - Pass mattress list to Matress section. - Implemented select mattress functionality. - Implemented matress section styles.

--- src/components/MattressSection/index.js
+++ src/components/MattressSection/index.js
@@ -1,10 +1,106 @@
-import { Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Box, Image, Heading, VStack, Button, Text } from "@chakra-ui/react";
 
-const MattressSection = () => {
+const MattressSection = ({ mattresses }) => {
+  const [selectedMattress, setSelectedMattress] = useState("");
+
+  useEffect(() => {
+    const keys = Object.keys(mattresses);
+    setSelectedMattress(keys[0]);
+    // eslint-disable-next-line
+  }, []);
+
+  const changeMattress = (key) => {
+    setSelectedMattress(key);
+  };
+
   return (
-    <Box display="flex" flexDirection={{ sm: "column", md: "row" }}>
-      <Box>Image</Box>
-      <Box>Button selects and such</Box>
+    <Box
+      display="flex"
+      flexDirection={{ base: "column", lg: "row" }}
+      justifyContent={{ base: "flex-start", lg: "center" }}
+      alignItems={{ base: "center", lg: "flex-start" }}
+      margin="0 auto"
+      height="100%"
+    >
+      <Box
+        display="flex"
+        flex={{ base: 0, lg: "1 0 300px" }}
+        justifyContent="center"
+        alignItems={{ base: "flex-start", lg: "flex-end" }}
+        padding="4"
+      >
+        {selectedMattress && (
+          <Image
+            src={
+              require(`assets/images/${mattresses[selectedMattress]?.imageFileName}`)
+                .default
+            }
+            alt="Matress Picture"
+          />
+        )}
+      </Box>
+
+      <Box
+        flex="1 1 300px"
+        justifyContent="center"
+        alignItems={{ base: "flex-start", lg: "flex-end" }}
+        padding="4"
+      >
+        <Heading>Choose Your Mattress</Heading>
+        <Box marginTop="12">
+          <Text fontSize="lg">SELECT MATTRESS TYPE</Text>
+        </Box>
+        <Box
+          display="flex"
+          marginTop="4"
+          width={{ base: "100%", lg: "400px" }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          {Object.keys(mattresses).map((key) => {
+            return (
+              <Button
+                flex="1"
+                key={key}
+                fontSize="md"
+                borderColor="gray"
+                borderWidth="1px"
+                borderRadius="0"
+                height="12"
+                onClick={() => changeMattress(key)}
+                backgroundColor={key === selectedMattress ? "gray" : "white"}
+                color={key === selectedMattress ? "white" : "gray"}
+              >
+                {mattresses[key].name}
+              </Button>
+            );
+          })}
+        </Box>
+        <Box
+          display="flex"
+          width={{ base: "100%", lg: "400px" }}
+          marginTop="12"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Text fontSize="md" fontWeight="bold">
+            {mattresses[selectedMattress].name} Mattress
+          </Text>
+          <Text>${mattresses[selectedMattress].price}</Text>
+        </Box>
+        <Box
+          display="flex"
+          width={{ base: "100%", lg: "400px" }}
+          marginTop="12"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+            <Button flex="1" borderRadius="0" backgroundColor="primary" color="white">
+                Add to Cart
+            </Button>
+        </Box>
+      </Box>
     </Box>
   );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,14 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import MattressSection from "components/MattressSection";
 import NavBar from "components/Nav";
+import mattresses from "api/mattresses";
 
 const Home = () => {
   return (
     <Box height="100vh" display="flex" flexDirection="column">
       <NavBar />
       <Box flex="1" width="100%" backgroundColor="background">
-        <MattressSection />
+        <MattressSection mattresses={mattresses.mattresses} />
       </Box>
     </Box>
   );
